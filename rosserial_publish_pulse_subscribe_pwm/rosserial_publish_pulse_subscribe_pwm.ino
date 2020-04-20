bccd56e19779b30e383d8b6dad49dab48bec569c
robot2 <benfduffy@gmail.com>
2020-04-20 21:08:35 +0200

    Many changes to make work with mega, but still need to do interrupts

--- rosserial_publish_pulse_subscribe_pwm/rosserial_publish_pulse_subscribe_pwm.ino
+++ rosserial_publish_pulse_subscribe_pwm/rosserial_publish_pulse_subscribe_pwm.ino
@@ -4,26 +4,27 @@
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int32.h>
 
-const int m1_in1 = 5;   // left
-const int m1_in2 = 7;
+const int m1_in1 = 27;   // left
+const int m1_in2 = 29;
 
-const int m2_in1 = 10;  // right
-const int m2_in2 = 11;
+const int m2_in1 = 23;  // right
+const int m2_in2 = 25;
 
 const int en1_pwm = 6;  // left
-const int en2_pwm = 9;  // right
+const int en2_pwm = 5;  // right
 
 long oldPositionLeft  = 0;
 long oldPositionRight  = 0;
-const int check_rpm_period = 20;
+//const int check_rpm_period = 20;
+const int check_rpm_period = 500;
 const int lowest_power_value = 50;
 
 // 8423 pulses per revolution by eye
 //int pulses_per_revolution = 8423;  // for old worm gear motors
 int pulses_per_revolution = 663;
 
-Encoder myEncLeft(2, 4);
-Encoder myEncRight(3, 12);
+Encoder myEncLeft(33, 31);
+Encoder myEncRight(37, 35);
 long oldPosition  = -999;
 unsigned long start_time = millis();
 unsigned long last_time_for_revolution = millis();
@@ -39,41 +40,40 @@ ros::Publisher rwheel_pub("rwheel", &rwheel_pulse_msg);
 // Float32? or int?
 
 void left_motor_cmd_callback( const std_msgs::Int16& cmd_msg){
-  Serial.println("Within left Callback");
-  digitalWrite(m1_in1, HIGH-digitalRead(m1_in1));
-  digitalWrite(m1_in2, LOW-digitalRead(m1_in2));
+  //Serial.println("Within left Callback");
+  /*digitalWrite(m1_in1, HIGH-digitalRead(m1_in1));
+  digitalWrite(m1_in2, LOW-digitalRead(m1_in2));*/
 
   if(cmd_msg.data > 0) {
     // todo delete or duplicate
     //motor_value = constrain(motor_value, 60, 255);
-    MotorCounterClockwiseLeft(abs(cmd_msg.data));
+    MotorClockwiseLeft(abs(cmd_msg.data));
   }
   else {
     //motor_value = constrain(motor_value, -255, -60);
-    MotorClockwiseLeft(abs(cmd_msg.data));
+    MotorCounterClockwiseLeft(abs(cmd_msg.data));
   }
 }
 
 void right_motor_cmd_callback( const std_msgs::Int16& cmd_msg){
-  Serial.println("Within right Callback");
+  //Serial.println("Within right Callback");
   /*digitalWrite(m2_in1, HIGH-digitalRead(m2_in1));
   digitalWrite(m2_in2, LOW-digitalRead(m2_in2));*/
 
   if(cmd_msg.data > 0) {
     //motor_value = constrain(motor_value, 60, 255);
-    MotorCounterClockwiseRight(abs(cmd_msg.data));
+    MotorClockwiseRight(abs(cmd_msg.data));
   }
   else {
     //motor_value = constrain(motor_value, -255, -60);
-    MotorClockwiseRight(abs(cmd_msg.data));
+    MotorCounterClockwiseRight(abs(cmd_msg.data));
   }
 }
 
 ros::Subscriber<std_msgs::Int16> rmotor_sub("lmotor_cmd", left_motor_cmd_callback);
 ros::Subscriber<std_msgs::Int16> lmotor_sub("rmotor_cmd", right_motor_cmd_callback);
 
-void setup(){
-  Serial.println("Hey everybody");
+void setup() {
   pinMode(m1_in1, OUTPUT);
   pinMode(m1_in2, OUTPUT);
   pinMode(m2_in1, OUTPUT);
@@ -81,13 +81,23 @@ void setup(){
   pinMode(en1_pwm, OUTPUT);
   pinMode(en2_pwm, OUTPUT);
 
-  digitalWrite(m1_in1, HIGH);
+  /*digitalWrite(m1_in1, HIGH);
   digitalWrite(m1_in2, LOW);
   digitalWrite(m2_in1, HIGH);
+  digitalWrite(m2_in2, LOW);*/
+
+  digitalWrite(m1_in1, LOW);
+  digitalWrite(m1_in2, LOW);
+  digitalWrite(m2_in1, LOW);
   digitalWrite(m2_in2, LOW);
 
-  analogWrite(en1_pwm, 0);
-  analogWrite(en2_pwm, 0);
+  // for testing
+  //analogWrite(en1_pwm, 80);
+  //analogWrite(en2_pwm, 80);  
+  //MotorClockwiseRight(110);
+  //MotorCounterClockwiseRight(80);
+  //MotorClockwiseLeft(60);
+  //MotorCounterClockwiseLeft(80);
 
   start_time = millis();
 
@@ -96,12 +106,9 @@ void setup(){
   nh.subscribe(rmotor_sub);
   nh.advertise(lwheel_pub);
   nh.advertise(rwheel_pub);
-  Serial.println("End of setup");
 }
 
-void loop(){
-  //analogWrite(en1_pwm, 130);
-  //analogWrite(en2_pwm, 130);
+void loop() {
   long curr_time = millis() - start_time;
   long time_difference = curr_time - prev_time;
   if (time_difference > check_rpm_period) {
@@ -128,10 +135,10 @@ void loop(){
 void MotorClockwiseLeft(int power) {
   //if(power > lowest_power_value) {
     analogWrite(en1_pwm, power);
-    /*digitalWrite(m1_in1, HIGH);
-    digitalWrite(m1_in2, LOW);*/
-    digitalWrite(m1_in1, HIGH-digitalRead(m1_in1));
-    digitalWrite(m1_in2, LOW-digitalRead(m1_in2));
+    digitalWrite(m1_in1, HIGH);
+    digitalWrite(m1_in2, LOW);
+    /*digitalWrite(m1_in1, HIGH-digitalRead(m1_in1));
+    digitalWrite(m1_in2, LOW-digitalRead(m1_in2));*/
   /*}
   else {
     digitalWrite(m1_in1, LOW);
@@ -159,10 +166,10 @@ void MotorCounterClockwiseLeft(int power) {
 void MotorClockwiseRight(int power) {
   if(power > lowest_power_value) {
     analogWrite(en2_pwm, power);
-    /*digitalWrite(m2_in1, HIGH);
-    digitalWrite(m2_in2, LOW);*/
-    digitalWrite(m2_in1, HIGH-digitalRead(m2_in1));
-    digitalWrite(m2_in2, LOW-digitalRead(m2_in2));
+    digitalWrite(m2_in1, HIGH);
+    digitalWrite(m2_in2, LOW);
+    /*digitalWrite(m2_in1, HIGH-digitalRead(m2_in1));
+    digitalWrite(m2_in2, LOW-digitalRead(m2_in2));*/
   }
   else {
     digitalWrite(m2_in1, LOW);
